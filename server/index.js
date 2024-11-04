114bcc3a23dc5761e72288e9ed59ab84e3cb9bcd
alok <alok.alokroy.roy@gmail.com>
2024-11-04 19:34:25 +0530

    .

---
+++ server/index.js
@@ -0,0 +1,67 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import PostRouter from "./routes/Posts.js";
+import GenerateImageRouter from "./routes/GenerateImage.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.use("/api/post", PostRouter);
+app.use("/api/generateImage", GenerateImageRouter);
+
+const PORT = process.env.PORT || 3000;
+
+//Default get
+//app.get("/", async (req, res) => {
+//  res.status(200).json({
+//    message: "Hello Developers!",
+//  });
+//});
+
+//function to connect to mongodb
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+      console.error("Failed to connect to DB");
+      console.error(err);
+    });
+};
+
+//function to start the server
+const startServer = async () => {
+  try {
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if ( process.env.NODE_ENV == "production"){
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+startServer();
